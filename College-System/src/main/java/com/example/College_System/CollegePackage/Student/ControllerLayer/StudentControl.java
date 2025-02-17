6d0e51c81275a878502083f64796f9ff8a8e5b3b
Mazen Khaled <64374092+mazen-khaled@users.noreply.github.com>
2025-02-17 20:53:42 +0200

    Add files via upload

---
+++ College-System/src/main/java/com/example/College_System/CollegePackage/Student/ControllerLayer/StudentControl.java
@@ -0,0 +1,41 @@
+package com.example.College_System.CollegePackage.Student.ControllerLayer;
+
+import com.example.College_System.CollegePackage.Student.DataBaseLayer.Student;
+import com.example.College_System.CollegePackage.Student.ServiceLayer.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/v1/students")
+public class StudentControl {
+    @Autowired
+    private StudentService studentService;
+
+    @GetMapping
+    public List<Student> getStudents(){
+        return studentService.getAllStudents();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Student> getStudentById(@PathVariable Long id){
+        return studentService.getStudentById(id);
+    }
+
+    @PostMapping
+    public void postNewStudent(@RequestBody Student student){
+        studentService.addStudent(student);
+    }
+
+    @PutMapping
+    public void updateStudent(@RequestBody Student student){
+        studentService.updateStudentData(student);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteStudentById(@PathVariable Long id){
+        studentService.deleteStudentById(id);
+    }
+}
